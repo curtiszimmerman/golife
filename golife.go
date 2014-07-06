9c3bc83a42d86b19d1b06a0c9af5fa3e9099bddf
curtiszimmerman <software@curtisz.com>
2014-07-06 01:32:29 -0700

    added: function stubs, version information

--- golife.go
+++ golife.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+const version = "v0.0.1a"
+
 func main() {
-	println("Welcome to the Game of Life")
+	fmt.Println("Welcome to the Game of Life " + version)
+}
+
+func tick() bool {
+	return false
+}
+
+func universe() bool {
+	return false
 }
